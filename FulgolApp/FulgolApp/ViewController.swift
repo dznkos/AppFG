a318a184da621d0602272944a50792ca83f98d4a
Goes <goes@Goess-Mac.local>
2017-12-14 14:52:32 -0500

    nose uqe poner

---
+++ FulgolApp/FulgolApp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  FulgolApp
+//
+//  Created by Goes on 12/14/17.
+//  Copyright © 2017 wips. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    // peru al Mundial 2020
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
